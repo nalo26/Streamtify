42309d44dd2fe7d050df4fc3dcf432b50b6873eb
nalo_ <me@nalo.dev>
2024-05-01 11:47:14 +0200

    ✨ Save content and cover to file

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
 import time
+from pathlib import Path
 
+import requests as rq
 import spotipy
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
 
+load_dotenv()
+
 SCOPE = "user-read-playback-state"
-REFRESH_RATE = os.getenv("REFRESH_RATE")
+REFRESH_RATE = int(os.getenv("REFRESH_RATE"))
+COVER_SIZE = int(os.getenv("COVER_SIZE"))
+
+OUTPUT_FORMAT = os.getenv("OUTPUT_FORMAT")
+OUTPUT_FOLDER = os.getenv("OUTPUT_FOLDER")
+OUTPUT_FILE = os.getenv("OUTPUT_FILE")
+OUTPUT_COVER = os.getenv("OUTPUT_COVER")
 
 
 def ms_to_time(ms):
@@ -16,6 +26,14 @@ def ms_to_time(ms):
 class Spotify:
     def __init__(self):
         self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
+        self.title: str = None
+        self.artist: str = None
+        self.current: int = None
+        self.duration: int = None
+        self.last_time: int = None
+        self.cover_link: str = None
+
+        self.last_output: str = None
 
     def listen(self):
         while True:
@@ -23,27 +41,45 @@ class Spotify:
             if track is None or track["is_playing"] is False:
                 print("No track playing")
             else:
-                self.print_track(track)
+                self.save_track(track)
+                self.export_track()
             time.sleep(REFRESH_RATE)
 
-    def print_track(self, track):
-        print(
-            '"{title}" - {artist} ({current}/{duration})'.format(
-                title=track["item"]["name"],
-                artist=", ".join([artist["name"] for artist in track["item"]["artists"]]),
-                current=ms_to_time(track["progress_ms"]),
-                duration=ms_to_time(track["item"]["duration_ms"]),
-            )
+    def save_track(self, track):
+        self.title = track["item"]["name"]
+        self.artist = ", ".join([artist["name"] for artist in track["item"]["artists"]])
+        self.current = track["progress_ms"]
+        self.duration = track["item"]["duration_ms"]
+        self.last_time = track["timestamp"]
+        self.cover_link = list(reversed(track["item"]["album"]["images"]))[COVER_SIZE]["url"]
+
+    def export_track(self):
+        output = OUTPUT_FORMAT.format(
+            TITLE=self.title,
+            ARTIST=self.artist,
+            CURRENT=ms_to_time(self.current),
+            DURATION=ms_to_time(self.duration),
         )
+
+        if output == self.last_output:
+            return
+        self.last_output = output
+
+        print(output)
+
+        with open(Path(OUTPUT_FOLDER) / OUTPUT_FILE, "w") as f:
+            f.write(output)
+
+        with open(Path(OUTPUT_FOLDER) / OUTPUT_COVER, "wb") as f:
+            f.write(rq.get(self.cover_link).content)
 
 
 if __name__ == "__main__":
-    load_dotenv()
     Spotify().listen()
 
 # TODO:
 # - [x] Try spotify api
 # - [x] get current track info
-# - [ ] save track info to file
-# - [ ] download image
+# - [x] save track info to file
+# - [x] download image
 # - [ ] progress bar ??
