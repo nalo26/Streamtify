e5d6a823be9344e2a7228da758b0eb786ce0124a
nalo_ <me@nalo.dev>
2024-11-03 19:12:09 +0100

    🐛 Fix run scripts installation commands

--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@
 
 if not exist ".venv" (
     python -m venv .venv
+    call .venv\Scripts\activate.bat
     python -m pip install -r requirements.txt
 )
 
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 if [ ! -d ".venv" ]; then
     python3 -m venv .venv
+    source .venv/bin/activate
     python3 -m pip install -r requirements.txt
 fi
 
